f00b7c1e88e64dba6c00be74eb7822d341871df5
tosmaster <tosmaster@gmail.com>
2019-06-07 00:37:44 -0700

    add train to run.sh
    Signed-off-by: tosmaster <tosmaster@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-python race.py --data_dir=RACE --bert_model=bert-large-uncased --output_dir=large_models --max_seq_length=320 --do_eval  --do_lower_case --train_batch_size=8 --eval_batch_size=32 --learning_rate=1e-5 --num_train_epochs=2 --gradient_accumulation_steps=8 --loss_scale=0
+python race.py --data_dir=RACE --bert_model=bert-large-uncased --output_dir=large_models --max_seq_length=320 --do_train --do_eval  --do_lower_case --train_batch_size=8 --eval_batch_size=32 --learning_rate=1e-5 --num_train_epochs=2 --gradient_accumulation_steps=8 --loss_scale=0
 
 
